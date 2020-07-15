103659da1a9167ce0fd00984496c61bad7954aab
Rismy Rally <rismyrally@gmail.com>
2020-07-16 01:56:28 +0530

    add(tableBody): extract tableBody from moviesTable component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
@@ -22,6 +23,7 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
+        <TableBody data={movies} />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
