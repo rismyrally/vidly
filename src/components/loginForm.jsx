c411ca6ae23f02c756fb4e0a78d446089929c1d8
Rismy Rally <rismyrally@gmail.com>
2021-03-06 13:52:23 +0530

    fix(login): handle input values

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' },
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
 
@@ -8,18 +12,40 @@ class LoginForm extends Component {
     console.log('Submitted');
   };
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='email' className='form-control' />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id='username'
+              name='username'
+              type='email'
+              className='form-control'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
-            <input id='password' type='password' className='form-control' />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id='password'
+              name='password'
+              type='password'
+              className='form-control'
+            />
           </div>
           <button className='btn btn-primary'>Login</button>
         </form>
