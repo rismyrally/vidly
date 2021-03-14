6ebbb4770b2d50c9c7e53327a66151c2c615679d
Rismy Rally <rismyrally@gmail.com>
2021-03-14 22:15:26 +0530

    Submit the Login Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Joi from 'joi';
 import Form from './common/form';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
   state = {
@@ -13,9 +14,9 @@ class LoginForm extends Form {
     password: Joi.string().required().label('Password'),
   });
 
-  doSubmit = () => {
-    // Call the server
-    console.log('Submitted');
+  doSubmit = async () => {
+    const { username, password } = this.state.data;
+    await login(username, password);
   };
 
   render() {
